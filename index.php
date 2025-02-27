27e5a93bd2aa1f805548b9fbb73b457dcfe2236c
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-02-27 11:41:11 +0100

    change in parking state, if is true or false

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>PHP Hotels</title>
 </head>
 <body>
@@ -50,16 +51,16 @@
 
     ];
 
-    echo var_dump($hotels);
+    // echo var_dump($hotels);
 
     foreach ($hotels as $hotel){
-        
+        echo "<ul>";
         foreach($hotel as $key => $value){
            
-            echo $key . ":" . "<br>" ;
+            echo "<li>" . $key . ": " . ($key === 'parking' ? ($value ? " c'è posto per parcheggiare" : " no c'è parcheggio") : $value )."<br>" . "</li>" ;
            
         }
-        echo  "<hr>";
+        echo  "</ul>" ."<hr>";
 
     };
     
